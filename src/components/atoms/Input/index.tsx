1ad89c066b2cc5074d00359784a28c1f6f266627
KenyaMasuko <ken12rug@gmail.com>
2022-07-29 07:20:49 +0900

    ✨ Atoms:インプットコンポーネントを作成

---
+++ src/components/atoms/Input/index.tsx
@@ -0,0 +1,44 @@
+import styled, { css } from "styled-components";
+
+//styled-componentの型は引数の型をジェネリクスで定義する
+//inputのコンポーネント
+const Input = styled.input<{ hasBorder?: boolean; hasError?: boolean }>`
+	color: ${({ theme }) => theme.colors.text};
+	${({ hasBorder, hasError, theme }) => {
+		if (hasBorder) {
+			return css`
+				border: 1px solid
+					${hasError ? theme.colors.danger : theme.colors.border};
+				border-radius: 5px;
+			`;
+		} else {
+			return css`
+				border: none;
+			`;
+		}
+	}}
+	padding: 11px 12px 12px 9px;
+	box-sizing: border-box;
+	outline: none;
+	width: 100%;
+	height: 38px;
+	font-size: 16px;
+	line-height: 19px;
+	&::placeholder {
+		color: ${({ theme }) => theme.colors.placeholder};
+	}
+	&::-webkit-outer-spin-button,
+	&::-webkit-inner-spin-button {
+		-webkit-appearance: none;
+		margin: 0;
+	}
+	&[type="number"] {
+		-moz-appearance: textfield;
+	}
+`;
+
+Input.defaultProps = {
+	hasBorder: true,
+};
+
+export { Input };
