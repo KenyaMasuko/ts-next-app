e01fda4c3652ed19abdd85a5d608d713bde1bcd0
KenyaMasuko <ken12rug@gmail.com>
2022-07-29 07:21:18 +0900

    ✨ Atoms:シェイプイメージコンポーネントを作成

---
+++ src/components/atoms/ShapeImage/index.tsx
@@ -0,0 +1,21 @@
+import Image, { ImageProps } from "next/image";
+import styled from "styled-components";
+
+//shapeImageのバリアント型
+type ImageShape = "circle" | "square";
+
+//shapeImageに渡すpropsの型
+type ShapeImageProps = ImageProps & { shape?: ImageShape };
+
+// circleなら円形に
+const ImageWidthShape = styled(Image)<{ shape?: ImageShape }>`
+	border-radius: ${({ shape }) => (shape === "circle" ? "50%" : "0")};
+`;
+
+//shapeImageのコンポーネント
+const ShapeImage = (props: ShapeImageProps) => {
+	const { shape, ...imageProps } = props;
+	return <ImageWidthShape shape={shape} {...imageProps} />;
+};
+
+export { type ImageShape, type ShapeImageProps, ShapeImage };
