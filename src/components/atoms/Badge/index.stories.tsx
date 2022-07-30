c4e4185a426278850549e628e8ba9744375eeb37
KenyaMasuko <ken12rug@gmail.com>
2022-07-30 12:22:44 +0900

    ✨  Atoms: Badgeを作成

---
+++ src/components/atoms/Badge/index.stories.tsx
@@ -0,0 +1,44 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { Badge } from ".";
+
+export default {
+	title: "Atoms/Badge",
+	argTypes: {
+		content: {
+			control: { type: "text" },
+			description: "バッジのテキスト",
+			table: {
+				type: { summary: "string" },
+			},
+		},
+		backgroundColor: {
+			control: { type: "color" },
+			description: "バッジの色",
+			table: {
+				type: { summary: "string" },
+			},
+		},
+	},
+} as ComponentMeta<typeof Badge>;
+
+const Template: ComponentStory<typeof Badge> = (args) => <Badge {...args} />;
+
+const Orange = Template.bind({});
+Orange.args = {
+	content: "1",
+	backgroundColor: "#ed9f28",
+};
+
+const Green = Template.bind({});
+Green.args = {
+	content: "2",
+	backgroundColor: "#32bf00",
+};
+
+const Red = Template.bind({});
+Red.args = {
+	content: "3",
+	backgroundColor: "#d4001a",
+};
+
+export { Orange, Green, Red };
---
+++ src/components/atoms/Badge/index.tsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+//バッジの周りのスタイリング
+const BadgeWrapper = styled.div<{ backgroundColor: string }>`
+	border-radius: 20px;
+	height: 20px;
+	min-width: 20px;
+	display: inline-flex;
+	align-items: center;
+	justify-content: center;
+	background-color: ${({ backgroundColor }) => backgroundColor};
+`;
+
+//バッジの中のテキストのスタイル
+const BadgeText = styled.p`
+	color: white;
+	font-size: 11px;
+	user-select: none;
+`;
+
+//バッジのpropsの型
+interface BadgeProps {
+	content: string;
+	backgroundColor: string;
+}
+
+//バッジコンポーネント
+const Badge = ({ content, backgroundColor }: BadgeProps) => {
+	return (
+		<BadgeWrapper backgroundColor={backgroundColor}>
+			<BadgeText>{content}</BadgeText>
+		</BadgeWrapper>
+	);
+};
+
+export { Badge };
