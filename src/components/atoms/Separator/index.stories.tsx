32dc266fdc7ce3aba23313953f9c151a21e9590c
KenyaMasuko <ken12rug@gmail.com>
2022-07-31 13:24:27 +0900

    ✨ Atoms: Separatorを作成

---
+++ src/components/atoms/Separator/index.stories.tsx
@@ -0,0 +1,16 @@
+import { ComponentMeta } from "@storybook/react";
+import { Separator } from ".";
+
+export default {
+	title: "Atoms/Separator",
+} as ComponentMeta<typeof Separator>;
+
+const Standard = () => {
+	<>
+		<Separator>or</Separator>
+		<Separator>and</Separator>
+		<Separator />
+	</>;
+};
+
+export { Standard };
---
+++ src/components/atoms/Separator/index.tsx
@@ -0,0 +1,34 @@
+import { ReactNode } from "react";
+import styled from "styled-components";
+
+//getMarginに渡す引数の型を定義
+interface SeparatorProps {
+	children?: ReactNode;
+}
+
+//marginのセット
+const getMargin = ({ children }: SeparatorProps) =>
+	children ? "0.50em" : "0em";
+
+const Separator = styled.div<SeparatorProps>`
+	height: 22px;
+	color: #e5e7eb;
+	display: flex;
+	align-items: center;
+	&::before,
+	&::after {
+		content: "";
+		flex: 1;
+		border-bottom: 1px solid #e5e7eb;
+	}
+
+	&::before {
+		margin-right: ${getMargin};
+	}
+
+	&::after {
+		margin-left: ${getMargin};
+	}
+`;
+
+export { Separator };
