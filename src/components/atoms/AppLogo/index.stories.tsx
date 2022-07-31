7d8ac5db070512477e0dc5e461c5d5d524473b48
KenyaMasuko <ken12rug@gmail.com>
2022-07-31 13:21:30 +0900

    ✨  Atoms: AppLogoを作成

---
+++ src/components/atoms/AppLogo/index.stories.tsx
@@ -0,0 +1,8 @@
+import { ComponentMeta } from "@storybook/react";
+import { AppLogo } from ".";
+
+export default {
+	title: "Atoms/AppLogo",
+} as ComponentMeta<typeof AppLogo>;
+
+export const Logo = () => <AppLogo />;
