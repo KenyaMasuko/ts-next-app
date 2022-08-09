25d5af9b46703c50bf460c2db11842a120684511
KenyaMasuko <ken12rug@gmail.com>
2022-08-10 07:58:37 +0900

    ✨ Organisms: ProductFormを実装

---
+++ src/components/Organisms/ProductForm/index.stories.tsx
@@ -0,0 +1,20 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { ProductForm } from ".";
+
+export default {
+	title: "Organisms/ProductForm",
+	artType: {
+		onProductSave: {
+			description: "商品ボタンを押した時のイベントハンドラ",
+			table: {
+				type: { summary: "function" },
+			},
+		},
+	},
+} as ComponentMeta<typeof ProductForm>;
+
+const Template: ComponentStory<typeof ProductForm> = (args) => (
+	<ProductForm {...args} />
+);
+
+export const Form = Template.bind({});
